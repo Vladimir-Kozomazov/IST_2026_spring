5b4912602d2b7677fe24f29febe4ab88618f3283
Vladimir-Kozomazov <voffkoz@gmail.com>
2026-04-02 09:39:42 +0300

    Update caesar.py

--- homework-02/caesar.py
+++ homework-02/caesar.py
@@ -1,20 +1,42 @@
 import typing as tp
-import string
 
 
-class caesar:
-    @staticmethod
-    def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-        shift = shift % 26
-        lower = string.ascii_lowercase
-        upper = string.ascii_uppercase
-        table = str.maketrans(
-            lower + upper, lower[shift:] + lower[:shift] + upper[shift:] + upper[:shift]
-        )
-        return plaintext.translate(table)
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
 
-    def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-        return caesar.encrypt_caesar(ciphertext, -shift)
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    ciphertext = ""
+    shift = shift % 26
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    table = str.maketrans(lower + upper, lower[shift:] + lower[:shift] + upper[shift:] + upper[:shift])
+    return plaintext.translate(table)
+
+
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    plaintext = ""
+    return caesar.encrypt_caesar(chiphertext, -shift)
 
 
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
