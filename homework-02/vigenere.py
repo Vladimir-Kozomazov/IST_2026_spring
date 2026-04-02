48dea2f390027f0a68ffb5a7d6a78f11727ec103
Vladimir-Kozomazov <voffkoz@gmail.com>
2026-04-02 09:41:06 +0300

    Update vigenere.py

--- homework-02/vigenere.py
+++ homework-02/vigenere.py
@@ -1,44 +1,51 @@
-import string
+import caesar
+def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+    """
+    Encrypts plaintext using a Vigenere cipher.
+
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+    ciphertext = ""
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    n = len(keyword)
+    cipher_text = list(plaintext)
+    for i in range(n):
+      if keyword[i] in lower:
+        shift = lower.find(keyword[i])
+      else:
+        shift = upper.find(keyword[i])
+      cipher_text[i::n] = caesar.encrypt_caesar(plaintext[i::n], shift)
+    cipher_text = "".join(cipher_text)
+    return cipher_text
 
 
-class caesar:
-    @staticmethod
-    def encrypt_caesar(plaintext, shift):
-        shift = shift % 26
-        lower = string.ascii_lowercase
-        upper = string.ascii_uppercase
-        table = str.maketrans(
-            lower + upper, lower[shift:] + lower[:shift] + upper[shift:] + upper[:shift]
-        )
-        return plaintext.translate(table)
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
 
-
-class vigenere:
-    @staticmethod
-    def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-        lower = string.ascii_lowercase
-        upper = string.ascii_uppercase
-        n = len(keyword)
-        cipher_text = list(plaintext)
-        for i in range(n):
-            if keyword[i] in lower:
-                shift = lower.find(keyword[i])
-            else:
-                shift = upper.find(keyword[i])
-            cipher_text[i::n] = caesar.encrypt_caesar(plaintext[i::n], shift)
-        cipher_text = "".join(cipher_text)
-        return cipher_text
-
-    def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-        lower = string.ascii_lowercase
-        upper = string.ascii_uppercase
-        n = len(keyword)
-        cipher_text = list(ciphertext)
-        for i in range(n):
-            if keyword[i] in lower:
-                shift = lower.find(keyword[i])
-            else:
-                shift = upper.find(keyword[i])
-            cipher_text[i::n] = caesar.encrypt_caesar(ciphertext[i::n], -shift)
-        cipher_text = "".join(cipher_text)
-        return cipher_text
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+    plaintext = ""
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    n = len(keyword)
+    plaintext = list(ciphertext)
+    for i in range(n):
+      if keyword[i] in lower:
+        shift = lower.find(keyword[i])
+      else:
+        shift = upper.find(keyword[i])
+      plaintext[i::n] = caesar.encrypt_caesar(ciphertext[i::n], -shift)
+    plaintext = "".join(plaintext)
+    return plaintext
